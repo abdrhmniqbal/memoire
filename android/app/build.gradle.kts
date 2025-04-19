3a28aa3fb84cf1979bafd19b1894a925bf0dc72a
Iqbal Abdurrahman <iqbalabdurrahman04@gmail.com>
2025-04-19 02:38:46 +0000

    feat: change icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,9 +31,9 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("debug")
         }
     }
